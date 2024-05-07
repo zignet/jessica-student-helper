147ec81671974cc2754046231a153ac25d53b37b
Eric Sigurdson <eric@zignet.com.au>
2024-05-07 12:50:16 +1000

    First Commit

---
+++ content/scripts/scripts.js
@@ -0,0 +1,13 @@
+function showPage(pageId) {
+    // Hide all pages
+    var pages = document.getElementsByClassName('page');
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].classList.remove('active');
+    }
+
+    // Show the selected page
+    var pageToShow = document.getElementById(pageId);
+    if (pageToShow) {
+        pageToShow.classList.add('active');
+    }
+}
